06fc872cd6ef0da333c864c4310b712847b874a5
syhsiao <shengying.hsiao@vpon.com>
2021-09-09 06:30:44 +0000

    Import Datastore of Java into store4s.v1.Datastore and create adding & deletion function

--- store4sV1/src/main/scala/store4s/v1/Datastore.scala
+++ store4sV1/src/main/scala/store4s/v1/Datastore.scala
@@ -1,3 +1,51 @@
 package store4s.v1
 
-case class Datastore(projectId: String, namespace: Option[String] = None)
+import com.google.datastore.v1.client.DatastoreFactory
+import com.google.datastore.v1.client.DatastoreHelper
+import com.google.datastore.v1.client.DatastoreOptions
+import com.google.datastore.v1.client.{Datastore => GDatastore}
+import com.google.datastore.v1.CommitRequest
+import com.google.datastore.v1.Entity
+import com.google.datastore.v1.Key
+import com.google.datastore.v1.Mutation
+
+case class Datastore(
+    options: DatastoreOptions,
+    namespace: Option[String] = None
+) {
+  val projectId: String = options.getProjectId()
+  val underlying: GDatastore = DatastoreFactory.get().create(options)
+
+  def add(entity: Entity) = {
+    val request = CommitRequest.newBuilder()
+      .addMutations(Mutation.newBuilder().setInsert(entity))
+      .setMode(CommitRequest.Mode.NON_TRANSACTIONAL)
+      .build()
+    underlying.commit(request).getMutationResults(0).getKey()
+  }
+
+  def delete(key: Key) = {
+    val request = CommitRequest.newBuilder()
+      .addMutations(Mutation.newBuilder().setDelete(key))
+      .setMode(CommitRequest.Mode.NON_TRANSACTIONAL)
+      .build()
+    underlying.commit(request).getMutationResults(0).getKey()
+  }
+
+  def delete(kind: String, name: String) = {
+    val key = DatastoreHelper.makeKey("Zombie", "heroine")
+    val request = CommitRequest.newBuilder()
+      .addMutations(Mutation.newBuilder().setDelete(key))
+      .setMode(CommitRequest.Mode.NON_TRANSACTIONAL)
+      .build()
+    underlying.commit(request).getMutationResults(0).getKey()
+  }
+}
+
+object Datastore {
+  def defaultInstance = Datastore(DatastoreHelper.getOptionsFromEnv().build())
+
+  def apply(projectId: String): Datastore = Datastore(
+    DatastoreHelper.getOptionsFromEnv().projectId(projectId).build()
+  )
+}
